94d867f9bd77e035a430839a838f773cf5f2304c
noki1213 <208437777+noki1213@users.noreply.github.com>
2026-01-27 13:33:45 +0900

    Initial Commit

---
+++ Axis/AxisApp.swift
@@ -0,0 +1,17 @@
+//
+//  AxisApp.swift
+//  Axis
+//
+//  Created by noki1213 on 2026/01/27.
+//
+
+import SwiftUI
+
+@main
+struct AxisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Axis/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Axis
+//
+//  Created by noki1213 on 2026/01/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
